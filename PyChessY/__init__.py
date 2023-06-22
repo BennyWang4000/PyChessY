ab81c655b74844f7393c230c62d59458becb268f
owner.laptop <wang.benny0102@gmail.com>
2023-06-22 13:44:10 +0800

    230622 prototype

---
+++ PyChessY/__init__.py
@@ -0,0 +1,1 @@
+from .chessy import ChessY
---
+++ PyChessY/utils/__init__.py
@@ -0,0 +1,5 @@
+from .structure import Place
+from .structure import PieceType
+from .structure import Move
+from .structure import Castling
+from .structure import Position
